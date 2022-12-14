7198addb93b836122fd84a070bdde99bfe42a9b3
Ronny Andrade <66749405+Ronny-Andrade@users.noreply.github.com>
2022-12-13 22:42:56 -0500

    user

--- Agapet/Agapet/settings.py
+++ Agapet/Agapet/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     #Models
+    'User',
 
 ]
 
@@ -132,7 +133,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
