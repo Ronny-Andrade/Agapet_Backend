b3027ac4bf97bd5584f5b73740d651558fd38f24
Ronny Andrade <66749405+Ronny-Andrade@users.noreply.github.com>
2022-12-17 11:02:25 -0500

    usuario

--- Backend/user/models.py
+++ Backend/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id= models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=100,unique=True)
+    direction = models.CharField(max_length=200)
+    phone = models.CharField(max_length=45)
+    age = models.PositiveIntegerField()
+    points = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='media/user', null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','phone','password']
+
+    def all_info_user(self):
+        return '{}'.format(self.user_id, self.name, self.email,self.direction,self.phone,self.age,self.points,self.image)
+
+    def __str__(self):
+        return self.all_info_user()
+    
+    class Meta:
+        verbose_name='User'
+        verbose_name_plural='Users'
+        db_table = 'user'
--- Backend/user/views.py
+++ Backend/user/views.py
@@ -1,3 +1,100 @@
+# Native
 from django.shortcuts import render
+# Django Rest Framework
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+import jwt, datetime
+# Serializers
+from .serializers import UserSerializer
+# Models
+from .models import User
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect Password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+
+        response.data = {
+            'jwt': token
+            }
+
+        return response
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms='HS256')
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+            }
+        return response
+
+class UserList(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+class Userid(APIView):
+    def get(self, request,pk):
+        user = User.objects.get(id=pk)
+        serializers = UserSerializer(user, many=False)
+        return Response(serializers.data)
+
+class Userupdate(APIView):
+    def put(self, request, pk):
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(instance=user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class Userdelete(APIView):
+    def delete(self, request, pk):
+        user = User.objects.get(id=pk)
+        user.delete()
+        return Response('Usuario eliminado satisfactoriamente')
